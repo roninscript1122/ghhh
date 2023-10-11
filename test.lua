c7ef506ffe9d77ee8d03a6e48bff8a5b4cf825e8
RONIN Script <126873854+roninscript1122@users.noreply.github.com>
2023-10-11 18:35:23 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,5 +1,4 @@
-function player_info(player, cmd)
-    outputChatBox("Name: #FFFFFF"..getPlayerName(player), player, 255, 0, 0, true)
-    outputChatBox("Money: #FFFFFF"..getPlayerMoney(player).."#00FF00$", player, 255, 0, 0, true)
+function player_info(player)
+    outputChatBox("สำเร็จ", player, 255, 255, 0, true)
 end
 addCommandHandler("info", player_info)

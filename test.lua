a72896b0a6b8db3444a1b2d3a1c6a1415852b621
RONIN Script <126873854+roninscript1122@users.noreply.github.com>
2023-10-31 20:05:24 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -5,4 +5,4 @@ function consoleCreateMarker ( playerSource, commandName )
 		outputChatBox ( "สร้างวงเเล้ว KimKie", playerSource )
 	end
 end
-addCommandHandler ( "cm", consoleCreateMarker )
+addCommandHandler ( "pon", consoleCreateMarker )
